0aaa1e7b9e452e61585bb07a820b3527613bbfe2
M-lapatack <124911873+M-lapatack@users.noreply.github.com>
2023-04-13 18:02:23 -0600

    made changes

---
+++ main.js
@@ -0,0 +1,20 @@
+// Complete the phrase exercise 
+
+// Button 1 "Once upon a... TIME"
+document.getElementById("btn1").addEventListener("click", btn1clicked)
+
+function btn1clicked() {
+    alert("time")
+}
+// Button 2 "You win some.. YOU LOSE SOME"
+document.getElementById("btn2").addEventListener("click", btn2clicked)
+
+function btn2clicked() {
+    alert("you lose some")
+}
+// Button 3 "WOO WOO... WEE WEE"
+document.getElementById("btn3").addEventListener("click", btn3clicked)
+
+function btn3clicked() {
+    alert("WEE WEE")
+}
